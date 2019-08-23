e18831c2cde2c9b0dfee7b6a23f08902d3cf833b
Ben Wicks <benjaminlwicks@gmail.com>
2019-08-23 16:55:29 -0500

    Set up book list

---
+++ app/src/main/java/com/exsilicium/wearesv/book/BookViewHolder.kt
@@ -0,0 +1,8 @@
+package com.exsilicium.wearesv.book
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class BookViewHolder(
+    itemView: View
+) : RecyclerView.ViewHolder(itemView)
